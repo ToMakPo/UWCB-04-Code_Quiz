c76142c998396c2cfadc5ae4024de2b52a46a306
Makai Post <postmakai@gmail.com>
2020-10-26 18:06:09 -0700

    added more questions

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -16,27 +16,92 @@ var clearHighscoresButton = document.getElementById('clear_highscores_button')
 
 // a list of questions to be asked
 const questions = [ // an array that holds the list of questions and their answers
+    // {
+    //     "question": "String values must be enclosed within ______ when being assigned to variables.",
+    //     "answers": ["commas", "curly brackets", "quotes", "parentheses"],
+    //     "correct_index": 2
+    // }, {
+    //     "question": "A very useful tool used during development and debugging for printing content to the debugger is:",
+    //     "answers": ["for loops", "console.log", "terminal / bash", "JavaScript"],
+    //     "correct_index": 1
+    // }, {
+    //     "question": "Commonly used data types DO NOT include:",
+    //     "answers": ["alerts", "numbers", "strings", "booleans"],
+    //     "correct_index": 0
+    // }, {
+    //     "question": "The condition in an if/else statement is enclosed within ______.",
+    //     "answers": ["quotes", "curly brackets", "square brackets", "parentheses"],
+    //     "correct_index": 3
+    // }
     {
-        "question": "String values must be enclosed within ______ when being assigned to variables.",
-        "answers": ["commas", "curly brackets", "quotes", "parentheses"],
-        "correct_index": 2
+        'question': 'String values must be enclosed within ______ when being assigned to variables.',
+        'answers': ['commas','curly brackets','quotes','parentheses'],
+        'correct_index': 2
     }, {
-        "question": "A very useful tool used during development and debugging for printing content to the debugger is:",
-        "answers": ["for loops", "console.log", "terminal / bash", "JavaScript"],
-        "correct_index": 1
+        'question': 'A very useful tool used during development and debugging for printing content to the debugger is:',
+        'answers': ['for loops','console.log','terminal / bash','JavaScript'],
+        'correct_index': 1
     }, {
-        "question": "Commonly used data types DO NOT include:",
-        "answers": ["alerts", "numbers", "strings", "booleans"],
-        "correct_index": 0
+        'question': 'Commonly used data types DO NOT include:',
+        'answers': ['alerts','numbers','strings','booleans'],
+        'correct_index': 0
     }, {
-        "question": "The condition in an if/else statement is enclosed within ______.",
-        "answers": ["quotes", "curly brackets", "square brackets", "parentheses"],
-        "correct_index": 3
+        'question': 'The condition in an if/else statement is enclosed within ______.',
+        'answers': ['quotes','curly brackets','square brackets','parentheses'],
+        'correct_index': 3
+    }, {
+        'question': 'Which program is used by web clients to view the web pages?',
+        'answers': ['Web browser','Protocol','Web server','Search Engine'],
+        'correct_index': 0
+    }, {
+        'question': 'The ______ attribute is used to identify the values of variables.',
+        'answers': ['text','http-equiv','content','name'],
+        'correct_index': 2
+    }, {
+        'question': 'Which tag is used to identify the keywords describing the site?',
+        'answers': ['Comment tag','Title tag','Meta tag','Anchor tag'],
+        'correct_index': 2
+    }, {
+        'question': 'Which are used with a tag to modify its function?',
+        'answers': ['Files','Functions','Attributes','Documents'],
+        'correct_index': 2
+    }, {
+        'question': 'This is a declaration that IS NOT an html tag.  it is an instruction to the web browser about what version of HTML a web page is written in.',
+        'answers': ['html','doctype','head','body'],
+        'correct_index': 1
+    }, {
+        'question': 'Content information that appears between the oppening and closing ______ tags will show up in a browser view.',
+        'answers': ['body','html','head','table'],
+        'correct_index': 0
+    }, {
+        'question': 'What tag element will display a line across the screen?',
+        'answers': ['br','a','hr','line'],
+        'correct_index': 2
+    }, {
+        'question': 'In a/an ______ list numbers and roman numerals can appear.',
+        'answers': ['Unordered','Ordered','Definition','Straight line'],
+        'correct_index': 1
+    }, {
+        'question': 'This is a language used for describing the look and formatting of a document written in markup language.',
+        'answers': ['HTML','CSS','JSON','XML'],
+        'correct_index': 1
+    }, {
+        'question': 'There are ______ characters in the hexidecimal color code combining color shades of red, green, and blue.',
+        'answers': ['4','5','6','7'],
+        'correct_index': 2
+    }, {
+        'question': 'Which HTML tag is used to communicate with search engines and describe the Web page\'s content?',
+        'answers': ['meta','html','body','head'],
+        'correct_index': 0
+    }, {
+        'question': 'Which term describes the skeletal layout of a Web page, without colors or graphics? ',
+        'answers': ['A wireframe','Mind map','Template','Goals'],
+        'correct_index': 0
     }
 ]
 
 // score tracking variables
-const startingTime = 120 // the amount of time that will be given the the user to answer all of the questions in seconds
+const startingTime = questions.length * 8 // the amount of time that will be given the the user to answer all of the questions in seconds
 const timePenalty = 10 // the amount of time that will be given the the user to answer all of the questions in seconds
 var remainingTime // the amount of time left on the clock
 var timer // the interval timer
